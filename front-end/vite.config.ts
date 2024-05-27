91f81ed0c7ed44d02f9271fafe3a56bbc387a825
Priyansh Tripathi <priyanshtripathi007@gmail.com>
2024-05-27 08:52:39 +0530

    fixed logo error

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  resolve: {
+    // alias: {
+    //   "@": path.resolve(__dirname, "./src"),
+    //   "@components": path.resolve(__dirname, "./src/components"),
+    //   "@utils/*": path.resolve(__dirname, "./src/utils/*"),
+    //   "@store/*": path.resolve(__dirname, "./src/store/*"),
+    //   "@hooks/*": path.resolve(__dirname,"./src/hooks/*"),
+    //   "@constants/*": path.resolve(__dirname,"./src/constants/*"),
+    //   "@assets/*": path.resolve(__dirname,"./src/assets/*"),
+    // },
   },
 });
